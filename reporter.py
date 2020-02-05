4c3a579280c69d0512a060655e69aa980b4ea831
Alex Castro <ac1950@georgetown.edu>
2020-02-05 18:07:10 -0500

    getting average grade

--- reporter.py
+++ reporter.py
@@ -11,15 +11,34 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 print("READING GRADEBOOK CSV FILE...")
+
+
 # if CSV file in same dir as this Python script:
 # csv_filepath = "gradebook2.csv"
 # if CSV file in the data dir:
 # csv_filepath = "data/gradebook.csv"
+
 csv_filepath = os.path.join(os.path.dirname(__file__), "data", "gradebook.csv")
 print("FILEPATH:", os.path.abspath(csv_filepath))
+
 grades = pandas.read_csv(csv_filepath)
 print("GRADES:", type(grades))
+
 #print(dir(grades))
 print(grades.tail())
+
 # grades["student_id"]
 # grades["final_grade"]
+
+#find avg grade
+
+grades_col = grades["final_grade"]
+print("GRADES COLUMN", type(grades_col))
+
+avg_grade = grades_col.mean()
+print("AVG GRADE: ", avg_grade)
+
+#LOOP through all rows
+
+
+#use for loop
